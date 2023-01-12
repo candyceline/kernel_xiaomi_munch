733faa966bd40c47bfffff1c30913cde8d3b312d
candyceline <candyceline07@gmail.com>
2023-01-12 04:26:32 +0000

    AnyKernel3 Setup From Danda
    Signed-off-by: candyceline <candyceline07@gmail.com>

--- AnyKernel3/anykernel.sh
+++ AnyKernel3/anykernel.sh
@@ -4,14 +4,14 @@
 ### AnyKernel setup
 # begin properties
 properties() { '
-kernel.string=ExampleKernel by osm0sis @ xda-developers
+kernel.string=Oxygen+ by Danda
 do.devicecheck=1
 do.modules=0
 do.systemless=1
 do.cleanup=1
 do.cleanuponabort=0
-device.name1=maguro
-device.name2=toro
+device.name1=munch
+device.name2=munchin
 device.name3=toroplus
 device.name4=tuna
 device.name5=
