45e5142a48fd58ee5b04946ff0d944b9b273e92d
Candy <candyceline07@gmail.com>
2023-02-04 07:19:27 +0000

    add script circleci
    Signed-off-by: Candy <candyceline07@gmail.com>

---
+++ .circleci/build.sh
@@ -0,0 +1,103 @@
+#!/usr/bin/env bash
+echo "Downloading few Dependecies . . ."
+git clone --depth=1 https://github.com/candyceline/kernel_xiaomi_munch -b Exclusive munch
+git clone --depth=1 https://android.googlesource.com/platform/prebuilts/clang/host/linux-x86 clang
+
+# Main
+KERNEL_NAME=Exclusive # IMPORTANT ! Declare your kernel name
+KERNEL_ROOTDIR=$(pwd)/munch # IMPORTANT ! Fill with your kernel source root directory.
+DEVICE_CODENAME=munch # IMPORTANT ! Declare your device codename
+DEVICE_DEFCONFIG=munch_defconfig # IMPORTANT ! Declare your kernel source defconfig file here.
+CLANG_ROOTDIR=$(pwd)/clang # IMPORTANT! Put your clang directory here.
+export KBUILD_BUILD_USER=candy # Change with your own name or else.
+export KBUILD_BUILD_HOST=fedora # Change with your own hostname.
+IMAGE=$(pwd)/munch/out/arch/arm64/boot/Image
+DATE=$(date +"%F-%S")
+START=$(date +"%s")
+
+# Checking environtment
+# Warning !! Dont Change anything there without known reason.
+function check() {
+echo ================================================
+echo KernelCompiler CircleCI
+echo version : ngntd
+echo ================================================
+echo BUILDER NAME = ${KBUILD_BUILD_USER}
+echo BUILDER HOSTNAME = ${KBUILD_BUILD_HOST}
+echo DEVICE_DEFCONFIG = ${DEVICE_DEFCONFIG}
+echo CLANG_VERSION = $(${CLANG_ROOTDIR}/bin/clang --version | head -n 1 | perl -pe 's/\(http.*?\)//gs' | sed -e 's/  */ /g')
+echo CLANG_ROOTDIR = ${CLANG_ROOTDIR}
+echo KERNEL_ROOTDIR = ${KERNEL_ROOTDIR}
+echo ================================================
+}
+
+# Compiler
+function compile() {
+
+   # Your Telegram Group
+   curl -s -X POST "https://api.telegram.org/bot$token/sendMessage" \
+        -d chat_id="$chat_id" \
+        -d "disable_web_page_preview=true" \
+        -d "parse_mode=html" \
+        -d text="<b>xKernelCompiler</b>%0ABUILDER NAME : <code>${KBUILD_BUILD_USER}</code>%0ABUILDER HOST : <code>${KBUILD_BUILD_HOST}</code>%0ADEVICE DEFCONFIG : <code>${DEVICE_DEFCONFIG}</code>%0ACLANG VERSION : <code>$(${CLANG_ROOTDIR}/bin/clang --version | head -n 1 | perl -pe 's/\(http.*?\)//gs' | sed -e 's/  */ /g')</code>%0ACLANG ROOTDIR : <code>${CLANG_ROOTDIR}</code>%0AKERNEL ROOTDIR : <code>${KERNEL_ROOTDIR}</code>"
+
+  cd ${KERNEL_ROOTDIR}
+  export PATH="$CLANG_DIR/bin:${PATH}"
+
+make O=out ARCH=arm64 $DEFCONFIG \
+    ARCH=arm64 \
+    LLVM=1 \
+    LLVM_IAS=1 \
+    CROSS_COMPILE=aarch64-linux-gnu- \
+    CROSS_COMPILE_ARM32=arm-linux-gnueabi-
+    
+make O=out \
+    ARCH=arm64 \
+    LLVM=1 \
+    LLVM_IAS=1 \
+    CROSS_COMPILE=aarch64-linux-gnu- \
+    CLANG_TRIPLE=aarch64-linux-gnu- \
+    CROSS_COMPILE_ARM32=arm-linux-gnueabi- \
+    -j${JTHREAD}
+
+   if ! [ -a "$IMAGE" ]; then
+	finerr
+	exit 1
+   fi
+    git clone --depth=1 -b main https://github.com/candyceline/AnyKernel3 AnyKernel3
+	cp out/arch/arm64/boot/Image AnyKernel3/Image.gz-dtb
+}
+
+# Push
+function push() {
+    cd AnyKernel3
+    ZIP=$(echo *.zip)
+    curl -F document=@$ZIP "https://api.telegram.org/bot$token/sendDocument" \
+        -F chat_id="$chat_id" \
+        -F "disable_web_page_preview=true" \
+        -F "parse_mode=html" \
+        -F caption="Compile took $(($DIFF / 60)) minute(s) and $(($DIFF % 60)) second(s). | For <b>Poco F4 (Munch)</b> | <b>$(${CLANG_ROOTDIR}/bin/clang --version | head -n 1 | perl -pe 's/\(http.*?\)//gs' | sed -e 's/  */ /g')</b>"
+
+}
+# Fin Error
+function finerr() {
+    curl -s -X POST "https://api.telegram.org/bot$token/sendMessage" \
+        -d chat_id="$chat_id" \
+        -d "disable_web_page_preview=true" \
+        -d "parse_mode=markdown" \
+        -d text="Build throw an error(s)"
+    exit 1
+}
+
+# Zipping
+function zipping() {
+    cd AnyKernel3 || exit 1
+    zip -r9 ${KERNEL_NAME}-${DEVICE_CODENAME}-${DATE}.zip *
+    cd ..
+}
+check
+compile
+zipping
+END=$(date +"%s")
+DIFF=$(($END - $START))
+push
